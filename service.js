d6f5528852c9b6149c59b9ebb93a4c6129e03815
srt4 <srt4@uw.edu>
2013-05-07 18:15:57 -0700

    correctly adding to mongo

--- service.js
+++ service.js
@@ -1,13 +1,42 @@
+// Dependencies
 var rest = require('restler');
-var sys = require('util');
 var mongo = require('mongodb');
+var config = require('./config');
 
-var options = {
+// Constants
+var OBA_API_URL = 'http://api.onebusaway.org/api/where/';
+var OBA_KEY = 'key=TEST';
+var OBA_URL_FACTORY = {
+    /**
+     * @return {string}
+     */
+    arrivalsAndDepartures: function(stopId) {
+        return OBA_API_URL + 'arrivals-and-departures-for-stop/' + stopId + '.json?' + OBA_KEY;
+    }
+};
+var OPTIONS = {
 		parser: rest.parsers.json
 };
+var DB = new mongo.Db(config.databaseName, new mongo.Server(config.databaseHost, config.databasePort));
+DB.open(function(err, db) {console.log(err); });
 
-rest.get('http://api.onebusaway.org/api/where/arrivals-and-departures-for-stop/1_68005.json?key=TEST', options).on('complete', function(data) {
-	
+var THREAD_POOL = [];
 
+// Core logic
+config.stopIds.forEach(function(stopId) {
+    var thread = setInterval(function() {
+        rest.get(OBA_URL_FACTORY.arrivalsAndDepartures(stopId), OPTIONS).on('complete', function(data) {
+            DB.collection('stops', function(err, collection) {
+                collection.insert(data, {safe: true}, function(err, records) {
+                    console.log(err);
+                    console.log(JSON.stringify(records));
+                });
+            });
+        });
+    }, config.refreshInterval);
+
+    THREAD_POOL.push(thread);
 });
+
+
 
